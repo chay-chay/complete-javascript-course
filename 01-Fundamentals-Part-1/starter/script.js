56e90b61a4c77af7147d8c3ca311c1d3214f3187
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-06-08 22:00:46 -0400

    10. Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,24 @@
-let js = 'amazing' 
-      if (js === 'amazing') alert ('JavaScript is FUN!')
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
 
-      
-      console.log(40 + 8 + 23 - 10)
+console.log("Chay");
+console.log(23);
+
+let firstName = "Janas";
+let first = "jonas";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+
+// convention 
+let jonas_matilda = "JM";
+let $function = 27;
+let person = "jonas";
+let PI = 3.1415;
+
+// Try to set name as understandable as you can
+let myFirstJob = "Programmer"
+let myCurrentjob = "Teacher"
+
+

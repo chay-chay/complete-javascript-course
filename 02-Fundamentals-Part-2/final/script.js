e11e1248718912c41f8ef8b7a5434760edaf971d
Chayanit <chayanit.chaisri@gmail.com>
2021-08-24 17:15:47 -0400

    Code challenge #3

--- 02-Fundamentals-Part-2/final/script.js
+++ 02-Fundamentals-Part-2/final/script.js
@@ -382,6 +382,44 @@ TEST DATA: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.9
 GOOD LUCK 😀
 */
 
+
+const mark = {
+  fullName: 'Mark Miller',
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2
+    return this.bmi
+  }
+}
+
+mark.calcBMI()
+const john = {
+  fullName: 'John Smith',
+  mass: 92,
+  height: 1.95,
+  calcBMI: function() {
+    this.bmi = this.mass / this.height **2
+    return this.bmi;
+  }
+}
+
+john.calcBMI()
+
+
+
+console.log("Mark", mark.bmi)
+console.log("John", john.bmi)
+
+console.log(`Mark Miller's BMI is higher than ${mark.bmi} John Smith's BMI ${john.bmi}  !`)
+
+if (mark.bmi > john.bmi){
+  console.log(`${mark.fullName}'s BMI is higher than ${mark.bmi} ${john.fullName}'s BMI ${john.bmi}  !`)
+}else {
+  console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`)
+}
+
+
 /*
 const mark = {
   fullName: 'Mark Miller',

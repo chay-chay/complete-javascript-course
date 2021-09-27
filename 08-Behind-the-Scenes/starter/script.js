f632add264f71f1c04a79775fd621fd65566f685
Chayanit <chayanit.chaisri@gmail.com>
2021-09-27 13:18:07 -0400

    Primitives vs. Objects (Primitive vs. Reference Types)

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log('age', age);
+console.log('oldAge', oldAge)
+
+const me ={
+    name: 'Jonas',
+    age: 30,
+}
+
+const friend = me
+friend.age = 27;
+console.log('Friend', friend);
+console.log('Me', me);
+
+// Primatives stored in call stack
+// Number
+// String
+// Boolean
+// Undefined
+// Null
+// Symbol
+// Bright
+
+// Object stored in HEAP
+// Object literal
+// Arrays
+// Functions
+// Many more ...

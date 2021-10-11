7d6c8375e8b613e7c7cbb873f94c8af45108da95
Chayanit McCormick <72841865+chay-chay@users.noreply.github.com>
2021-10-11 00:26:59 -0400

    81. Handling an Esc Keypress Event

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -7,10 +7,10 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 // console.log(btnsOpenModal);
 
-const openModal = function() {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-}
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 const closeModal = function () {
   modal.classList.add('hidden');
@@ -18,9 +18,15 @@ const closeModal = function () {
 };
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', openModal )
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
-
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log('A key was pressed', e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});

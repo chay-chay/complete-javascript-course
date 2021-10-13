d01c00d08e6444d4999d5dbe32bbfe41dccea781
Chayanit McCormick <72841865+chay-chay@users.noreply.github.com>
2021-10-12 22:25:35 -0400

    Coding Challenge #1

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -56,3 +56,32 @@ const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 //   const amplitude = calcTempAmplitude(temperatures);
 //   console.log(amplitude);
 
+///////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+*/
+
+function printForecast(test1) {
+    let  result =""
+    let day = 1
+    for (  let index in test1 ){
+        result  += `${test1[index]} ºC in ${day} days... `
+        day++
+    }
+    return result
+}
+
+const test1 = [17, 21, 23]
+const test2 = [12, 5, -5, 0, 4]
+console.log(printForecast(test1)) 
